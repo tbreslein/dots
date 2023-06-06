b964ad09acaa51185b2def3a276ceb92d680e4fe
tbreslein <tommy.breslein@protonmail.com>
2023-06-06 21:21:27 +0200

    clean up nvim config, most importantly lsp

--- config/nvim/.config/nvim/lua/lsp-config.lua
+++ config/nvim/.config/nvim/lua/lsp-config.lua
@@ -1,194 +1,133 @@
-require("mason-update-all").setup({})
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
-local lsp = require("lsp-zero").preset({
-    name = "minimal",
-    set_lsp_keymaps = { preserve_mappings = false },
-    manage_nvim_cmp = false,
-    suggest_lsp_servers = false,
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+local lspconfig = require("lspconfig")
+
+lspconfig.ansiblels.setup({ capabilites = capabilities })
+lspconfig.astro.setup({ capabilites = capabilities })
+lspconfig.fortls.setup({ capabilites = capabilities })
+lspconfig.html.setup({ capabilites = capabilities })
+lspconfig.pyright.setup({ capabilites = capabilities })
+lspconfig.tsserver.setup({ capabilites = capabilities })
+lspconfig.rust_analyzer.setup({ capabilites = capabilities })
+lspconfig.yamlls.setup({ capabilites = capabilities })
+lspconfig.zls.setup({ capabilites = capabilities })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+		require("which-key").register({
+			l = {
+				name = "LSP actions",
+				r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "rename" },
+				f = { "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "async format" },
+				c = { "<cmd>lua vim.lsp.buf.code_action" },
+			},
+		}, {
+			prefix = "<leader>",
+		})
+		require("which-key").register({
+			g = {
+				name = "go to (LSP)",
+				d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "definition" },
+				D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "declaration" },
+				i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "implementation" },
+				t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "type def" },
+				r = { "<cmd>Telescope lsp_references<cr>", "lsp references" },
+				s = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "signature help" },
+				l = { "<cmd>lua vim.diagnostic.open_float()<cr>", "diagnostic float" },
+				n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "next diagnostic" },
+				p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "prev diagnostic" },
+			},
+		})
+	end,
 })
 
-lsp.on_attach(function(_, bufnr)
-    lsp.default_keymaps({ buffer = bufnr })
-    require("which-key").register({
-        l = {
-            name = "LSP actions",
-            r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "rename" },
-            f = { "<cmd>lua vim.lsp.buf.format({async = true})<cr>", "async format" },
-        },
-    }, {
-        prefix = "<leader>",
-    })
-    require("which-key").register({
-        g = {
-            name = "go to (LSP)",
-            d = { "<cmd>lua vim.lsp.buf.definition()<cr>", "definition" },
-            D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "declaration" },
-            i = { "<cmd>lua vim.lsp.buf.implementation()<cr>", "implementation" },
-            t = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "type def" },
-            r = { "<cmd>Telescope lsp_references<cr>", "lsp references" },
-            s = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "signature help" },
-            l = { "<cmd>lua vim.diagnostic.open_float()<cr>", "diagnostic float" },
-            n = { "<cmd>lua vim.diagnostic.goto_next()<cr>", "next diagnostic" },
-            p = { "<cmd>lua vim.diagnostic.goto_prev()<cr>", "prev diagnostic" },
-        },
-    })
-end)
+local cmp = require("cmp")
+cmp.setup({
+	snippet = {
+		expand = function(args)
+			require("luasnip").lsp_expand(args.body)
+		end,
+	},
+	window = {
+		completion = cmp.config.window.bordered(),
+		documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Tab>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<C-y>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+	}, {
+		{ name = "buffer" },
+	}),
+})
 
-lsp.nvim_workspace()
-local rust_lsp = lsp.build_options("rust_analyzer", {})
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
 
--- servers that are installed globally and only need to be setup
-lsp.setup_servers({ "ccls", "pylsp", "zls" })
+cmp.setup.cmdline({ ":" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
--- local nc = null_ls.builtins.code_actions
 local nd = null_ls.builtins.diagnostics
 local nf = null_ls.builtins.formatting
 null_ls.setup({
-    sources = {
-        -- c/c++
-        nd.cppcheck,
-        nf.clang_format,
-        -- js/ts
-        nd.eslint,
-        nf.prettierd.with({
-            extra_filetypes = { "svelte", "toml" },
-        }),
-        nd.tsc.with({ prefer_local = "node_modules/.bin" }),
-        -- lua
-        nf.stylua,
-        nd.luacheck.with({ extra_args = { "--globals", "vim" } }),
-        -- shell, docker, config files, etc
-        nd.shellcheck,
-        nd.ansiblelint,
-        nd.hadolint,
-        -- nf.shellharden,
-        -- python
-        nd.ruff,
-        nf.black,
-        -- rust
-        nf.rustfmt,
-        -- zig
-        nf.zigfmt,
-    },
-    -- on_attach = null_opts.on_attach,
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                    -- vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
-})
-
-lsp.setup()
-
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-
-local cmp = require("cmp")
-local cmp_mappings = lsp.defaults.cmp_mappings()
-cmp_mappings["<Tab>"] = nil
-cmp_mappings["<S-Tab>"] = nil
-cmp_mappings["<CR>"] = nil
-local cmp_sources = lsp.defaults.cmp_sources()
-table.insert(cmp_sources, { name = "lua-latex-symbols", option = { cache = true } })
-
-local icons = {
-    Class = "",
-    Constant = "",
-    Constructor = "",
-    Field = "",
-    Function = "",
-    Keyword = "",
-    Method = "",
-    Module = "",
-    Snippet = "󰅴",
-    Text = "",
-    Variable = "󰭷",
-
-    nvim_lsp = "LSP",
-    buffer = "Buf",
-    luasnip = "Snp",
-    latex_symbols = "TeX",
-}
-
-local cmp_config = lsp.defaults.cmp_config({
-    window = {
-        completion = cmp.config.window.bordered(),
-    },
-    mappings = cmp_mappings,
-    sources = cmp_sources,
-    formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-            local function trim(text, max)
-                if text and text:len() > max then
-                    text = text:sub(1, max) .. "..."
-                end
-                return text
-            end
-
-            vim_item.kind = icons[vim_item.kind] or vim_item.kind
-
-            local source = entry.source.name
-            if source == "lua-latex-symbols" then
-                source = "latex_symbols"
-            end
-
-            vim_item.menu = " [" .. (icons[source] or source) .. "]"
-
-            if source == "luasnip" or source == "nvim_lsp" then
-                vim_item.dup = 0
-            end
-
-            vim_item.abbr = trim(vim_item.abbr, 70)
-            return vim_item
-        end,
-    },
-})
-cmp.setup(cmp_config)
-
--- lsp.setup_nvim_cmp({
--- 	mapping = cmp_mappings,
--- 	sources = cmp_sources,
--- })
-
--- restore some diagnostics settings
-vim.diagnostic.config({
-    virtual_text = true,
-})
-
-require("rust-tools").setup({
-    server = rust_lsp,
-    tools = { inlay_hints = { only_current_line = true } },
-})
-
--- `/` cmdline setup.
-cmp.setup.cmdline("/", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = {
-        { name = "buffer" },
-    },
-})
-
--- `:` cmdline setup.
-cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline(),
-    sources = cmp.config.sources({
-        { name = "path" },
-    }, {
-        {
-            name = "cmdline",
-            option = {
-                ignore_cmds = { "Man", "!" },
-            },
-        },
-    }),
+	sources = {
+		-- c/c++
+		nd.cppcheck,
+		nf.clang_format,
+		-- js/ts
+		nd.eslint,
+		nf.prettierd.with({
+			extra_filetypes = { "svelte", "toml" },
+		}),
+		nd.tsc.with({ prefer_local = "node_modules/.bin" }),
+		-- lua
+		nf.stylua,
+		nd.luacheck.with({ extra_args = { "--globals", "vim" } }),
+		-- shell, docker, config files, etc
+		nd.shellcheck,
+		nd.ansiblelint,
+		nd.hadolint,
+		-- nf.shellharden,
+		-- python
+		nd.ruff,
+		nf.black,
+		-- rust
+		nf.rustfmt,
+		-- zig
+		nf.zigfmt,
+	},
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
 })
--- config/nvim/.config/nvim/lua/plugins.lua
+++ config/nvim/.config/nvim/lua/plugins.lua
@@ -1,300 +1,237 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    "mrjones2014/legendary.nvim",
-    {
-        "aserowy/tmux.nvim",
-        config = function()
-            return require("tmux").setup({
-                copy_sync = { enable = false },
-            })
-        end,
-    },
-    {
-        "folke/which-key.nvim",
-        config = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-            require("which-key").setup({})
-        end,
-    },
-    -- UI
-    "kdheepak/lazygit.nvim",
-    {
-        "sainnhe/gruvbox-material",
-        config = function()
-            vim.o.termguicolors = true
-            vim.o.background = "dark"
-            vim.g.gruvbox_material_background = "hard"
-            vim.g.gruvbox_material_foreground = "material"
-            vim.g.gruvbox_material_enable_italic = 1
-            vim.g.gruvbox_material_enable_bold = 1
-            vim.g.gruvbox_material_transparent_background = 1
-            vim.g.gruvbox_material_sign_column_background = "none"
-            vim.g.gruvbox_material_ui_contrast = "high"
-            vim.g.gruvbox_material_statusline_style = "mix"
-            vim.cmd("colorscheme gruvbox-material")
-        end,
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "kyazdani42/nvim-web-devicons" },
-        config = function()
-            require("lualine").setup({
-                options = {
-                    globalstatus = true,
-                    theme = "gruvbox-material",
-                    component_separators = "",
-                    section_separators = "",
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch", "diff", "diagnostics" },
-                    lualine_c = { "filename" },
-                    lualine_x = { "" },
-                    lualine_y = { "progress" },
-                    lualine_z = { "location" },
-                },
-            })
-        end,
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("indent_blankline").setup()
-        end,
-    },
-    {
-        "rcarriga/nvim-notify",
-        config = function()
-            vim.notify = require("notify")
-            vim.o.termguicolors = true
-            require("notify").setup({
-                background_colour = "#000000",
-            })
-        end,
-    },
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("todo-comments").setup()
-        end,
-    },
-    {
-        "lewis6991/gitsigns.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("gitsigns").setup()
-        end,
-    },
-    {
-        "sindrets/diffview.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("diffview").setup()
-        end,
-    },
-    "uga-rosa/ccc.nvim",
-    {
-        "j-hui/fidget.nvim",
-        config = function()
-            require("fidget").setup({})
-        end,
-    },
+	"mrjones2014/legendary.nvim",
+	{
+		"aserowy/tmux.nvim",
+		config = function()
+			return require("tmux").setup({
+				copy_sync = { enable = false },
+			})
+		end,
+	},
+	{
+		"folke/which-key.nvim",
+		config = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+			require("which-key").setup({})
+		end,
+	},
+	-- UI
+	{
+		"sainnhe/gruvbox-material",
+		config = function()
+			vim.o.termguicolors = true
+			vim.o.background = "dark"
+			vim.g.gruvbox_material_background = "hard"
+			vim.g.gruvbox_material_foreground = "material"
+			vim.g.gruvbox_material_enable_italic = 1
+			vim.g.gruvbox_material_enable_bold = 1
+			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_sign_column_background = "none"
+			vim.g.gruvbox_material_ui_contrast = "high"
+			vim.g.gruvbox_material_statusline_style = "mix"
+			vim.cmd("colorscheme gruvbox-material")
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "kyazdani42/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					globalstatus = true,
+					theme = "gruvbox-material",
+					component_separators = "",
+					section_separators = "",
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch" },
+					lualine_c = { "diagnostics" },
+					lualine_x = { "" },
+					lualine_y = { { "filename", path = 3 } },
+					lualine_z = { "progress" },
+				},
+			})
+		end,
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("indent_blankline").setup()
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("todo-comments").setup()
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
 
-    {
-        "nvim-treesitter/nvim-treesitter",
-        dependencies = { "nvim-treesitter/nvim-treesitter-context" },
-        build = function()
-            vim.cmd("TSUpdate")
-        end,
-        config = function()
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "vimdoc",
-                    "javascript",
-                    "typescript",
-                    "tsx",
-                    "css",
-                    "html",
-                    "astro",
-                    "json",
-                    "toml",
-                    "yaml",
-                    "bash",
-                    "gitignore",
-                    "haskell",
-                    "python",
-                    "c",
-                    "cpp",
-                    "cmake",
-                    "make",
-                    "markdown",
-                    "lua",
-                    "rust",
-                    "zig",
-                },
-                sync_install = false,
-                auto_install = true,
-                highlight = {
-                    enable = true,
-                    additional_vim_regex_highlighting = false,
-                },
-                autotag = {
-                    enable = true,
-                },
-            })
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		dependencies = { "nvim-treesitter/nvim-treesitter-context" },
+		build = function()
+			vim.cmd("TSUpdate")
+		end,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"vimdoc",
+					"javascript",
+					"typescript",
+					"tsx",
+					"css",
+					"html",
+					"astro",
+					"json",
+					"toml",
+					"yaml",
+					"bash",
+					"gitignore",
+					"haskell",
+					"python",
+					"c",
+					"cpp",
+					"cmake",
+					"make",
+					"markdown",
+					"lua",
+					"rust",
+					"zig",
+				},
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+				autotag = {
+					enable = true,
+				},
+			})
+		end,
+	},
 
-    -- movement
-    {
-        "ggandor/leap.nvim",
-        config = function()
-            require("leap").opts.highlight_unlabeled_phase_one_targets = true
-        end,
-    },
-    {
-        "theprimeagen/harpoon",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("telescope").setup({
-                file_ignore_patterns = {
-                    "vendor/*",
-                    "%.lock",
-                    "__pycache__/*",
-                    "%.sqlite3",
-                    "%.ipynb",
-                    "node_modules/*",
-                    "%.jpg",
-                    "%.jpeg",
-                    "%.png",
-                    "%.svg",
-                    "%.otf",
-                    "%.ttf",
-                    ".git/",
-                    "%.webp",
-                    ".dart_tool/",
-                    ".github/",
-                    ".gradle/",
-                    ".idea/",
-                    ".settings/",
-                    ".vscode/",
-                    "__pycache__/",
-                    "build/",
-                    "env/",
-                    "gradle/",
-                    "node_modules/",
-                    "target/",
-                    "%.pdb",
-                    "%.dll",
-                    "%.class",
-                    "%.exe",
-                    "%.cache",
-                    "%.ico",
-                    "%.pdf",
-                    "%.dylib",
-                    "%.jar",
-                    "%.docx",
-                    "%.met",
-                    "smalljre_*/*",
-                    ".vale/",
-                },
-            })
-        end,
-    },
-    {
-        "stevearc/oil.nvim",
-        config = function()
-            require("oil").setup({})
-        end,
-    },
+	-- movement
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").opts.highlight_unlabeled_phase_one_targets = true
+		end,
+	},
+	{
+		"theprimeagen/harpoon",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				file_ignore_patterns = {
+					"vendor/*",
+					"%.lock",
+					"__pycache__/*",
+					"%.sqlite3",
+					"%.ipynb",
+					"node_modules/*",
+					"%.jpg",
+					"%.jpeg",
+					"%.png",
+					"%.svg",
+					"%.otf",
+					"%.ttf",
+					".git/",
+					"%.webp",
+					".dart_tool/",
+					".github/",
+					".gradle/",
+					".idea/",
+					".settings/",
+					".vscode/",
+					"__pycache__/",
+					"build/",
+					"env/",
+					"gradle/",
+					"node_modules/",
+					"target/",
+					"%.pdb",
+					"%.dll",
+					"%.class",
+					"%.exe",
+					"%.cache",
+					"%.ico",
+					"%.pdf",
+					"%.dylib",
+					"%.jar",
+					"%.docx",
+					"%.met",
+					"smalljre_*/*",
+					".vale/",
+				},
+			})
+		end,
+	},
+	{
+		"stevearc/oil.nvim",
+		config = function()
+			require("oil").setup({})
+		end,
+	},
 
-    -- editing
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup({})
-        end,
-    },
-    "windwp/nvim-ts-autotag",
-    "mbbill/undotree",
-    "gpanders/editorconfig.nvim",
-    {
-        "nvim-pack/nvim-spectre",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("spectre").setup({})
-        end,
-    },
-    {
-        "smjonas/inc-rename.nvim",
-        config = function()
-            require("inc_rename").setup({})
-        end,
-    },
+	-- editing
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({})
+		end,
+	},
+	"windwp/nvim-ts-autotag",
+	"mbbill/undotree",
+	"gpanders/editorconfig.nvim",
+	{
+		"smjonas/inc-rename.nvim",
+		config = function()
+			require("inc_rename").setup({})
+		end,
+	},
 
-    -- languages
-    "NoahTheDuke/vim-just",
-    "elkowar/yuck.vim",
-    {
-        "MunifTanjim/rust-tools.nvim",
-        branch = "patched",
-        config = function()
-            require("rust-tools").setup({ tools = { inlay_hints = { only_current_line = true } } })
-        end,
-    },
-    -- {
-    --     "simrat39/rust-tools.nvim",
-    --     config = function()
-    --         require("rust-tools").setup({ tools = { inlay_hints = { only_current_line = true } } })
-    --     end,
-    -- },
+	-- languages
+	"NoahTheDuke/vim-just",
+	"elkowar/yuck.vim",
 
-    -- LSP
-    "jose-elias-alvarez/null-ls.nvim",
-    "rubixdev/mason-update-all",
-    {
-        "folke/trouble.nvim",
-        dependencies = { "kyazdani42/nvim-web-devicons" },
-        config = function()
-            require("trouble").setup({})
-        end,
-    },
+	-- LSP
+	"neovim/nvim-lspconfig",
+	"hrsh7th/cmp-nvim-lsp",
+	"hrsh7th/cmp-buffer",
+	"hrsh7th/cmp-path",
+	"hrsh7th/cmp-cmdline",
+	"hrsh7th/nvim-cmp",
+	"amarakon/nvim-cmp-lua-latex-symbols",
 
-    -- LSP zero
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            "neovim/nvim-lspconfig",
-            "williamboman/mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/nvim-cmp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "saadparwaiz1/cmp_luasnip",
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-cmdline",
-            "amarakon/nvim-cmp-lua-latex-symbols",
-            "L3MON4D3/LuaSnip",
-            "rafamadriz/friendly-snippets",
-        },
-    },
+	"L3MON4D3/LuaSnip",
+	"saadparwaiz1/cmp_luasnip",
+
+	"jose-elias-alvarez/null-ls.nvim",
 })
