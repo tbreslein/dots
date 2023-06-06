6e783e1e7d6457fbc9f7135a90874bb2b64eefef
tbreslein <tommy.breslein@protonmail.com>
2023-06-06 11:50:52 +0200

    decrease colorcolumn position in nvim

--- config/nvim/.config/nvim/lua/settings.lua
+++ config/nvim/.config/nvim/lua/settings.lua
@@ -2,7 +2,7 @@
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
-vim.opt.colorcolumn = "120"
+vim.opt.colorcolumn = "100"
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 vim.opt.termguicolors = true
