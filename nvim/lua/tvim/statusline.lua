335aa8ac2f94dcc4cb494a2c750e169521d353ad
tbreslein <tommy.breslein@protonmail.com>
2024-04-05 21:20:18 +0200

    (fresh-nvim) add statusline

---
+++ nvim/lua/tvim/statusline.lua
@@ -0,0 +1,28 @@
+local M = {
+    "nvim-lualine/lualine.nvim",
+}
+
+function M.config()
+    require("lualine").setup({
+        options = { component_separators = "", section_separators = "" },
+        sections = {
+            lualine_a = { "mode" },
+            -- lualine_b = {},
+            lualine_b = { "branch", "diff" },
+            lualine_c = { { "filename", path = 3 } },
+            lualine_x = { "diagnostics" },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+        },
+    })
+end
+
+return M
