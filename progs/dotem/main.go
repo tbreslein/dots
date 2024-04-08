50df16b69f8e9526d587e941765aef3e50e391eb
tbreslein <tommy.breslein@protonmail.com>
2024-04-08 12:18:35 +0200

    basic godm setup works!

--- progs/dotem/main.go
+++ progs/dotem/main.go
@@ -4,20 +4,25 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("hello world")
-	// Find .env file
-	err := godotenv.Load(".env")
+	bin_path, err := os.Executable()
+	if err != nil {
+		log.Fatalf("unable to locate executable: %s", err)
+	}
+	bin_path = filepath.Dir(bin_path)
+
+	err = godotenv.Load(filepath.Join(bin_path, ".env"))
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err)
 	}
 
 	// Getting and using a value from .env
-	greeting := os.Getenv("ROLES")
+	roles := os.Getenv("ROLES")
 
-	fmt.Println(greeting)
+	fmt.Println(roles)
 }
