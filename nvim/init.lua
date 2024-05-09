48b0d89a9aa395b8998402c37397275330f850c6
tbreslein <tommy.breslein@protonmail.com>
2024-05-09 15:01:49 +0200

    clean up

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,8 +37,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.mouse = "a"
 vim.opt.fileencoding = "utf-8"
--- vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
-vim.opt.clipboard = "unnamedplus"
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 vim.opt.laststatus = 3
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
@@ -46,7 +45,7 @@ vim.opt.fillchars = { eob = " " }
 vim.opt.inccommand = "split"
 vim.opt.autoread = true
 vim.g.my_obsidian_vault = os.getenv("HOME") .. "/syncthing/notes/vault"
-vim.g.my_dotfiles = os.getenv("HOME") .. "/dotfiles"
+vim.g.my_dotfiles = os.getenv("HOME") .. "/dots"
 vim.fn.setenv("CC", "gcc")
 vim.fn.setenv("CXX", "g++")
 
