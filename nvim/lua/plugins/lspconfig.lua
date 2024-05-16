89221fa6272a0f4261fe81fe09202e1b1b89b7ed
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-16 16:07:14 +0200

    add lspconfig

---
+++ nvim/lua/plugins/lspconfig.lua
@@ -0,0 +1,142 @@
+require("trouble").setup()
+local lspconfig = require("lspconfig")
+local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            -- require("luasnip").lsp_expand(args.body)
+            vim.snippet.expand(args.body)
+        end,
+    },
+    window = { documentation = cmp.config.window.bordered() },
+    mapping = cmp.mapping.preset.insert({
+        ["<c-p>"] = cmp.config.disable,
+        ["<c-f>"] = cmp.config.disable,
+        ["<c-b>"] = cmp.config.disable,
+        ["<c-j>"] = cmp.mapping.select_next_item(select_opts),
+        ["<c-k>"] = cmp.mapping.select_prev_item(select_opts),
+        ["<c-l>"] = cmp.mapping.confirm({ select = true }),
+        ["<c-n>"] = cmp.mapping(cmp.mapping.scroll_docs(-4)),
+        ["<c-m>"] = cmp.mapping(cmp.mapping.scroll_docs(4)),
+        ["<Del>"] = cmp.mapping(function(fallback)
+            if luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    enabled = function()
+        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt"
+    end,
+    formatting = {
+        format = function(entry, vim_item)
+            local kind = require("lspkind").cmp_format({
+                mode = "symbol_text",
+                maxwidth = 40,
+            })(entry, vim_item)
+            local strings = vim.split(kind.kind, "%s", { trimempty = true })
+            kind.kind = " " .. (strings[1] or "") .. " "
+            kind.menu = "    (" .. (strings[2] or "") .. ")"
+            return kind
+        end,
+    },
+    sources = {
+        { name = "path" },
+        { name = "nvim_lsp", keyword_length = 1 },
+        { name = "buffer", keyword_length = 3 },
+        { name = "luasnip" },
+        { name = "nvim_lsp_signature_help" },
+    },
+})
+local cmp_cmdline_mappings = {
+    ["<c-p>"] = cmp.config.disable,
+    ["<c-j>"] = {
+        c = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+    },
+    ["<c-k>"] = {
+        c = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+    },
+}
+cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(cmp_cmdline_mappings),
+    sources = { { name = "buffer" } },
+})
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(cmp_cmdline_mappings),
+    sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+lspconfig.bashls.setup({ capabilities = lsp_capabilities })
+lspconfig.dockerls.setup({ capabilities = lsp_capabilities })
+lspconfig.gopls.setup({ capabilities = lsp_capabilities })
+lspconfig.marksman.setup({ capabilities = lsp_capabilities })
+lspconfig.tsserver.setup({ capabilities = lsp_capabilities })
+lspconfig.pyright.setup({
+    capabilities = lsp_capabilities,
+    on_new_config = function(config, root_dir)
+        local env =
+            vim.trim(vim.fn.system('cd "' .. (root_dir or ".") .. '"; poetry env info --executable 2>/dev/null'))
+        if string.len(env) > 0 then
+            config.settings.python.pythonPath = env
+        end
+    end,
+})
+lspconfig.lua_ls.setup({
+    capabilities = lsp_capabilities,
+    settings = {
+        Lua = {
+            runtime = { version = "LuaJIT", path = vim.split(package.path, ";") },
+            diagnostics = { globals = { "vim" } },
+            workspace = {
+                library = {
+                    [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+                    [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+                },
+            },
+        },
+    },
+})
+lspconfig.clangd.setup({ capabilities = lsp_capabilities })
+lspconfig.neocmake.setup({ capabilities = lsp_capabilities })
+lspconfig.nil_ls.setup({ capabilities = lsp_capabilities })
+lspconfig.zls.setup({ capabilities = lsp_capabilities })
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+
+vim.diagnostic.config({
+    signs = { active = true },
+    update_in_insert = false,
+    underline = true,
+    severity_sort = true,
+    float = {
+        border = "rounded",
+        source = true,
+        header = "",
+        prefix = "",
+    },
+})
