7f8e10dece8ac0feb2b0847cce18941d4a739f17
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-21 09:18:51 +0200

    nvim/fix dap listeners

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,21 +1,17 @@
-local dap = require("dap")
-local dapui = require("dapui")
-dapui.setup()
-dap.listeners.before.attach["dapui_config"] = function()
+local dap, dapui = require("dap"), require("dapui")
+dap.listeners.before.attach.dapui_config = function()
     dapui.open()
 end
-dap.listeners.launch.attach["dapui_config"] = function()
+dap.listeners.before.launch.dapui_config = function()
     dapui.open()
 end
-dap.listeners.after.event_initialized["dapui_config"] = function()
-    dapui.open()
-end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.event_terminated.dapui_config = function()
     dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited.dapui_config = function()
     dapui.close()
 end
+dap.setup()
 
 require("nvim-dap-virtual-text").setup()
 
