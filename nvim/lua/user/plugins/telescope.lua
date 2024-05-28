c8b5a0cb2fa4279eeb2888b0260ae53e38f5ab3a
Tommy Breslein <tommy.breslein@protonmail.com>
2024-05-28 11:47:48 +0200

    fix telescope layout

--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -10,28 +10,30 @@ local M = {
 function M.config()
   local telescope = require "telescope"
   local builtin = require "telescope.builtin"
-  local dropdown = require("telescope.themes").get_dropdown {}
   telescope.setup()
   telescope.load_extension "undo"
   telescope.load_extension "zf-native"
 
+  local layout_defaults =
+    { layout_strategy = "vertical", layout_config = { width = 0.8 } }
+
   kmap("n", "<leader>ff", function()
     vim.fn.system "git rev-parse --is-inside-work-tree"
     if vim.v.shell_error == 0 then
-      builtin.git_files(dropdown)
+      builtin.git_files(layout_defaults)
     else
-      builtin.find_files(dropdown)
+      builtin.find_files(layout_defaults)
     end
   end)
   kmap("n", "<leader>fg", function()
-    builtin.git_files(dropdown)
+    builtin.git_files(layout_defaults)
   end)
   kmap("n", "<leader>fs", function()
-    builtin.live_grep(dropdown)
+    builtin.live_grep(layout_defaults)
   end)
   kmap("n", "<leader>u", ":Telescope undo<cr>")
   kmap("n", "<leader>gs", function()
-    builtin.git_branches(dropdown)
+    builtin.git_branches(layout_defaults)
   end)
 end
 
