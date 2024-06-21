231085127e04592fee38b26f8aa25e1f50fd0f64
Tommy Breslein <tommy.breslein@protonmail.com>
2024-06-21 08:01:51 +0200

    nvim/remove kind.menu in cmp-popups

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -68,7 +68,8 @@ function M.config()
         }(entry, vim_item)
         local strings = vim.split(kind.kind, "%s", { trimempty = true })
         kind.kind = " " .. (strings[1] or "") .. " "
-        kind.menu = "    (" .. (strings[2] or "") .. ")"
+        -- kind.menu = "    (" .. (strings[2] or "") .. ")"
+        kind.menu = ""
         return kind
       end,
     },
