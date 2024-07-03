8ecd405db9f9beca97918b2ae29f89c4afcc95a3
Tommy Breslein <tommy.breslein@protonmail.com>
2024-07-03 21:22:45 +0200

    move editorconfig to home manager

--- modules/coding.nix
+++ modules/coding.nix
@@ -4,15 +4,45 @@
   pkgs,
   pkgs-stable,
   ...
-}: let
-  cfg = config.coding;
-in {
+}:
+# let
+#   cfg = config.coding;
+# in
+{
   options = {
     roles.enableCoding = lib.mkEnableOption "Enable coding role";
-    coding.defaultTmuxConfig = lib.mkEnableOption "Use default tmux, instead of custom config";
+    coding.customTmuxConfig = lib.mkEnableOption "Use custom tmux config";
   };
 
   config = lib.mkIf config.roles.enableCoding {
     home.packages = (with pkgs-stable; [tmux jq jqp]) ++ (with pkgs; [neovim]);
+
+    editorconfig = {
+      enable = true;
+      settings = {
+        "*" = {
+          charset = "utf-8";
+          indent_size = 4;
+          indent_style = "space";
+          max_line_width = 80;
+          trim_trailing_whitespace = true;
+        };
+        "*.{nix,cabal,hs,lua}" = {
+          indent_size = 2;
+        };
+        "*.{json,js,jsx,ts,tsx,cjs,mjs}" = {
+          indent_size = 2;
+        };
+        "*.{yml,yaml,ml,mli,hl,md,mdx,html,astro}" = {
+          indent_size = 2;
+        };
+        "CMakeLists.txt" = {
+          indent_size = 2;
+        };
+        "{m,M}akefile" = {
+          indent_style = "tab";
+        };
+      };
+    };
   };
 }
